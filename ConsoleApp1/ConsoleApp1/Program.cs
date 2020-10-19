81c0c26c433bca400a1035253a53dad570e06868
Harrison Wallace (1000039866) <WALLHM1@student.op.ac.nz>
2020-10-20 12:48:59 +1300

    add a line of text

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("octy fluffy what now?");
+            Console.ReadLine();
+
         }
     }
 }
